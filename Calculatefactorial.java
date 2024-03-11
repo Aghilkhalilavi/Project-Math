59697b206a7c27af9e21c69a7828570578bbba4f
Aghi <Aghilkh76@gmail.com>
2024-03-11 01:49:07 -0700

    creat MultiplicationTable

--- Calculatefactorial.java
+++ Calculatefactorial.java
@@ -5,8 +5,9 @@ public class Calculatefactorial {
   public static void main(String[] args) {
     int n=(new Scanner(System.in).nextInt());
     int fact=1;
-    for(int i=1;i<=n;i++)
+    for(int i=1;i<=n;i++){
     fact=fact*i;
+    }
     System.out.println(n+"!="+fact);
   }
 }
