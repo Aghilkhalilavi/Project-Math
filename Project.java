0dbdd94d03b780d2d570007b0f02b8480c45783e
Aghi <Aghilkh76@gmail.com>
2024-03-11 04:29:38 -0700

    creat and changed

--- Project.java
+++ Project.java
@@ -1,10 +1,11 @@
-import java.util.Scanner;
-
 public class Project {
-public static void main(String[] args) {
-    System.out.println("hi");
-}
 
-
+    public static void main(String[] args) {
+       for(int i=1;i<=1;i++){
+    for(int j=1;i<=j;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+       }
+    }
 }
- 
