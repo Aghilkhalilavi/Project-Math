15038c86f6c8e529c341e5882ac55e73eef4831c
Aghi <Aghilkh76@gmail.com>
2024-03-11 04:31:36 -0700

    Calculatefactorial

--- Calculatefactorial.java
+++ Calculatefactorial.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Calculatefactorial {
 
   public static void main(String[] args) {
-    int n=(new Scanner(System.in).nextInt());
-    int fact=1;
+    long n=(new Scanner(System.in).nextLong());
+    long fact=1;
     for(int i=1;i<=n;i++){
     fact=fact*i;
     }
